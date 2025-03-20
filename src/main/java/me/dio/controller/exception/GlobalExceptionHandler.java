6065a26e970a481b1f1ecf47f5a665371cf9ab9e
RodrigoEmerson <rodrigo_belon@hotmail.com>
2025-03-20 00:03:32 -0300

    Tentanto implementar Spring BD no RailWay

---
+++ src/main/java/me/dio/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+/*
+package me.dio.controller.exception;
+
+
+
+import me.dio.domain.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity<String> handlerBusinessException(IllegalAccessException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handlerNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("ID do recurso não encontrado ", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handlerUnexpectedException(Throwable unexpectedException) {
+        var message = "Erro inesperado do servidor, veja os logs ";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+*/
